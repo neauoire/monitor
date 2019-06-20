51b997f1c8a8995efb8cf6e7f8254c612b5036a9
Devine Lu Linvega <aliceffekt@gmail.com>
2019-06-20 21:42:45 +0900

    Some progress

--- sequencer.lua
+++ sequencer.lua
@@ -7,6 +7,8 @@
 --   \\\\////
 --
 
+engine.name = 'OutputTutorial'
+
 local midi_signal_in
 local midi_signal_out
 local viewport = { width = 128, height = 64, frame = 0 }
@@ -14,6 +16,8 @@ local root = 60
 local pattern = { length = 6, cells = {} }
 local focus = { id = 1, sect = 1 }
 local playhead = { id = 1, sect = 1, is_playing= true }
+
+local mode = 0
 
 -- Main
 
@@ -44,7 +48,9 @@ end
 function run()
   if playhead.is_playing ~= true then return end
   playhead.id = (viewport.frame % pattern.length) + 1
+  playhead.sect = get_sect(playhead.id)+1
   viewport.frame = viewport.frame + 1
+  engine.hz(note_to_hz(get_output()))
   redraw()
 end
 
@@ -74,6 +80,33 @@ function get_output()
   return root + get_mod()
 end
 
+function move(delta)
+  if delta == 1 then
+    if focus.sect == #get_cell(focus.id) then
+      move_cell(delta)
+    else
+      move_sect(delta)
+    end
+  end
+  
+  if delta == -1 then
+    if focus.sect == 1 then
+      move_cell(delta)
+    else
+      move_sect(delta)
+    end
+  end
+end
+
+function move_cell(delta)
+  focus.id = clamp(focus.id + delta,1, pattern.length)
+  focus.sect = clamp(focus.sect, 1, #get_cell(focus.id))
+end
+
+function move_sect(delta)
+  focus.sect = clamp(focus.sect + delta, 1, #get_cell(focus.id))
+end
+
 function reset_cells()
   pattern.cells = {}
   pattern.cells[1]  = { 0 }
@@ -100,15 +133,50 @@ function mod_cell(id,sect,size)
   pattern.cells[id+1] = { 1, 2, 1, 3}
 end
 
+function mod_length(delta)
+  current = #pattern.cells[focus.id]
+  if delta == 1 then
+    if current == 1 then
+      pattern.cells[focus.id] = { pattern.cells[focus.id][1], pattern.cells[focus.id][1] }
+    elseif current == 2 then
+      pattern.cells[focus.id] = { pattern.cells[focus.id][1], pattern.cells[focus.id][2], pattern.cells[focus.id][1], pattern.cells[focus.id][2] }
+    end
+  end
+  if delta == -1 then
+    if current == 4 then
+      pattern.cells[focus.id] = { pattern.cells[focus.id][1], pattern.cells[focus.id][2] }
+    elseif current == 2 then
+      pattern.cells[focus.id] = { pattern.cells[focus.id][1] }
+    end
+  end
+end
+
 -- Interactions
 
 function key(id,state)
+  -- Swap modes
+  if state == 0 then
+    mode = 0
+  else
+    mode = id
+  end
   redraw()
 end
 
 function enc(id,delta)
+  if mode == 3 then
+    mod_length(delta)
+    redraw()
+    return
+  end
   if id == 1 then
     pattern.length = clamp(pattern.length + delta, 1, 16)
+  end
+  if id == 2 then
+    move(delta)
+  end
+  if id == 3 then
+    pattern.cells[focus.id][focus.sect] = clamp(pattern.cells[focus.id][focus.sect] + delta,-12,12)
   end
   redraw()
 end
@@ -117,18 +185,21 @@ end
 
 function draw_cell_content(id,x,y)
   if #get_cell(id) == 1 then
-    if id == playhead.id then screen.level(1) ; screen.rect(_x,_y,8,8) ; screen.fill() end
+    if id == playhead.id then screen.level(15) ; screen.rect(_x,_y,8,8) ; screen.fill() end
+    if id == focus.id then screen.level(1) ; screen.rect(_x,_y,8,8) ; screen.fill() end
     screen.level(15)
-    for sect = 1,8 do screen.pixel(x-1+sect,y+7+get_mod(id,1)) end
+    for sect = 1,8 do screen.pixel(x-1+sect,y+7-get_mod(id,1)) end
   end
   if #get_cell(id) == 2 then
-    if id == playhead.id then screen.level(1) ; screen.rect(x+(get_sect(id)*4),_y,4,8) ; screen.fill() end
+    if id == playhead.id then screen.level(15) ; screen.rect(x+(get_sect(id)*4),_y,4,8) ; screen.fill() end
+    if id == focus.id then screen.level(1) ; screen.rect(x+((focus.sect-1)*4),_y,4,8) ; screen.fill() end
     screen.level(15)
     for sect = 1,4 do screen.pixel(x-1+sect,y+7-get_mod(id,1)) end
     for sect = 1,4 do screen.pixel(x-1+4+sect,y+7-get_mod(id,2)) end
   end
   if #get_cell(id) == 4 then
-    if id == playhead.id then screen.level(1) ; screen.rect(x+(get_sect(id)*2),_y,2,8) ; screen.fill() end
+    if id == playhead.id then screen.level(15) ; screen.rect(x+(get_sect(id)*2),_y,2,8) ; screen.fill() end
+    if id == focus.id then screen.level(1) ; screen.rect(x+((focus.sect-1)*2),_y,2,8) ; screen.fill() end
     screen.level(15)
     for sect = 1,2 do screen.pixel(x-1+sect,y+7-get_mod(id,1)) end
     for sect = 1,2 do screen.pixel(x-1+2+sect,y+7-get_mod(id,2)) end
@@ -155,23 +226,46 @@ function draw_sequencer()
 end
 
 function draw_labels()
+  x_pad = 10
   screen.level(15)
-  screen.move(60,22)
-  screen.text('?')
-  screen.move(60,31)
-  screen.text('mod: '..get_mod())
-  screen.move(60,40)
+  if mode == 3 then
+    screen.move(60,22)
+    screen.text('DIV')
+  else
+    -- Focus
+    screen.move(60+(x_pad*0),22)
+    screen.text(focus.id)
+    screen.move(60+(x_pad*1),22)
+    screen.text(pattern.length)
+    screen.move(60+(x_pad*2),22)
+    screen.text(get_sect(focus.id)+1)
+    
+    screen.move(60+(x_pad*3),22) -- length
+    screen.text(#get_cell(focus.id))
+    screen.move(60+(x_pad*4),22) 
+    screen.text(get_mod(focus.id,focus.sect))
+  end
+  -- 
+  screen.move(60+(x_pad*0),31)
   screen.text(playhead.id)
-  screen.move(70,40)
+  screen.move(60+(x_pad*1),31)
   screen.text(pattern.length)
-  screen.move(80,40)
+  screen.move(60+(x_pad*2),31)
   screen.text(get_sect(playhead.id)+1)
-  screen.move(90,40)
+  
+  screen.move(60+(x_pad*3),31) -- length
   screen.text(#get_cell(playhead.id))
+  screen.move(60+(x_pad*4),31)
+  screen.text(get_mod(playhead.id,playhead.sect))
+  -- 
   screen.move(60,49)
-  screen.text(note_to_format(get_input()))
-  screen.move(80,49)
-  screen.text(note_to_format(get_output()))
+  screen.text(note_to_name(get_input()))
+  screen.move(60+(x_pad*1),49)
+  screen.text(note_to_octave(get_input()))
+  screen.move(60+(x_pad*2),49)
+  screen.text(note_to_name(get_output()))
+  screen.move(60+(x_pad*3),49)
+  screen.text(note_to_octave(get_output()))
 end
 
 function redraw()
