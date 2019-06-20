b6400b0d096469cb9543766767c87c3a16b227f9
Devine Lu Linvega <aliceffekt@gmail.com>
2019-06-20 20:26:02 +0900

    *

--- sequencer.lua
+++ sequencer.lua
@@ -48,18 +48,22 @@ function run()
   redraw()
 end
 
+function get_cell(id)
+  return pattern.cells[id]
+end
+
 function get_mod(id,sect)
   id = id or playhead.id
   sect = sect or playhead.sect
-  return pattern.cells[id][sect]
+  return get_cell(id)[sect]
 end
 
 function get_sect(id)
-  return math.floor(viewport.frame/pattern.length) % pattern.cells[id].sect
+  return math.floor(viewport.frame/pattern.length) % #get_cell(id)
 end
 
 function get_sect_width(id)
-  return get_sect(id) * (4/pattern.cells[id].sect)
+  return get_sect(id) * (4/#get_cell(id))
 end
 
 function get_input()
@@ -92,7 +96,6 @@ function reset_cells()
 end
 
 function mod_cell(id,sect,size)
-  
   pattern.cells[id] = { 1, 2}
   pattern.cells[id+1] = { 1, 2, 1, 3}
 end
@@ -113,15 +116,20 @@ end
 -- Render
 
 function draw_cell_content(id,x,y)
-  screen.level(15)
-  if #pattern.cells[id] == 1 then
+  if #get_cell(id) == 1 then
+    if id == playhead.id then screen.level(1) ; screen.rect(_x,_y,8,8) ; screen.fill() end
+    screen.level(15)
     for sect = 1,8 do screen.pixel(x-1+sect,y+7+get_mod(id,1)) end
   end
-  if #pattern.cells[id] == 2 then
+  if #get_cell(id) == 2 then
+    if id == playhead.id then screen.level(1) ; screen.rect(x+(get_sect(id)*4),_y,4,8) ; screen.fill() end
+    screen.level(15)
     for sect = 1,4 do screen.pixel(x-1+sect,y+7-get_mod(id,1)) end
     for sect = 1,4 do screen.pixel(x-1+4+sect,y+7-get_mod(id,2)) end
   end
-  if #pattern.cells[id] == 4 then
+  if #get_cell(id) == 4 then
+    if id == playhead.id then screen.level(1) ; screen.rect(x+(get_sect(id)*2),_y,2,8) ; screen.fill() end
+    screen.level(15)
     for sect = 1,2 do screen.pixel(x-1+sect,y+7-get_mod(id,1)) end
     for sect = 1,2 do screen.pixel(x-1+2+sect,y+7-get_mod(id,2)) end
     for sect = 1,2 do screen.pixel(x-1+4+sect,y+7-get_mod(id,3)) end
@@ -136,12 +144,6 @@ function draw_cell(id,x,y)
   y = math.floor((id-1) / 4)
   _x = (x * 9) + 3 + 8
   _y = (y * 9) + 7 + 8
-  -- Background
-  if id == playhead.id then
-    screen.level(1)
-    screen.rect(_x,_y,8,8)
-    screen.fill()
-  end
   -- Content
   draw_cell_content(id,_x,_y)
 end
@@ -157,11 +159,19 @@ function draw_labels()
   screen.move(60,22)
   screen.text('?')
   screen.move(60,31)
-  screen.text('hello')
+  screen.text('mod: '..get_mod())
   screen.move(60,40)
-  screen.text(playhead.id..' mod: '..get_mod())
+  screen.text(playhead.id)
+  screen.move(70,40)
+  screen.text(pattern.length)
+  screen.move(80,40)
+  screen.text(get_sect(playhead.id)+1)
+  screen.move(90,40)
+  screen.text(#get_cell(playhead.id))
   screen.move(60,49)
-  screen.text(note_to_format(get_input())..' > '..note_to_format(get_output()))
+  screen.text(note_to_format(get_input()))
+  screen.move(80,49)
+  screen.text(note_to_format(get_output()))
 end
 
 function redraw()
@@ -210,7 +220,7 @@ end
 -- Timer
 
 re = metro.init()
-re.time = 1.25
+re.time = 0.25
 re.event = function()
   run()
 end
